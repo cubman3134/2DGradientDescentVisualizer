ef95204afe20308b85c25bc24e60f4a94f4d17e7
Parker Holt <cubman3134@gmail.com>
2019-04-16 21:02:34 -0400

    Update GradientDescentVisualizer.py

--- GradientDescentVisualizer.py
+++ GradientDescentVisualizer.py
@@ -2,18 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def func_y(x):
-    y = 50 * np.sin(x) + (x ** 2)
+    y = np.sin(x)
     return y
 
-cur_x = 7
-rate = 0.1
+cur_x = 1.5889
+rate = 0.01
 precision = 0.000001
 previous_step_size = 1
 max_iters = 1000
 iters = 0
-df = lambda x: 50*np.cos(x)+2*x
+df = lambda x: np.cos(x)
 
-x = np.arange(-10, 10, 0.01)
+x = np.arange(0, 20, 0.01)
 y = func_y(x)
 init = cur_x
 fig, ax = plt.subplots()
@@ -29,5 +29,5 @@ while previous_step_size > precision and iters < max_iters:
     iters = iters + 1
     plt.scatter(cur_x, func_y(cur_x), s=20)
     print("Iteration", iters, "\nX value is", cur_x)
-plt.legend(["Step Size = " + str(rate)])
+plt.legend(["Step Size = " + str(rate) + " minX: " + str(round(cur_x, 4)) + " minY: " + str(round(func_y(cur_x), 4))])
 plt.savefig("x" + str(init) + "r" + str(rate) + "gradiant.png")
